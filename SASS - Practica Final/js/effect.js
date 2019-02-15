4e6b1b415b55c8529dd48d8d26a7b53549343e4c
unknown <victorst79@gmail.com>
2019-02-15 22:33:48 +0100

    practica hecha de nuevo, la anterior la va hacer supu

--- SASS - Practica Final/js/effect.js
+++
@@ -1,9 +0,0 @@
-const pageable = new Pageable("main", {
-	animation: 400,
-	delay: 300,
-	onBeforeStart: function() {
-		this.pages.forEach((page, i) => {
-			page.classList.remove("pg-active");
-		});	
-	}	
-});
